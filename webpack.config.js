7cb4672a829415c4c4dd8ccd255ccd8ef4dbeed5
_xiaotian <952822399@qq.com>
2018-12-08 01:30:29 +0800

    webpack

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const webpack = require('webpack')
+const path = require('path')
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    })
+  ]
+}
